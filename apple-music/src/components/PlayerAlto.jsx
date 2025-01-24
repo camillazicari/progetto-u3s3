629923b48aa09b7c97a65968162a3976df813ea4
camillazicari <camyzicari01@gmail.com>
2025-01-24 17:04:21 +0100

    icone

--- apple-music/src/components/PlayerAlto.jsx
+++ apple-music/src/components/PlayerAlto.jsx
@@ -8,9 +8,9 @@ const PlayerAlto = () => {
     >
       <div className="text-white-50 d-flex align-items-center">
         <i className="bi bi-shuffle fs-6 px-1"></i>
-        <i className="bi bi-skip-backward-fill fs-4 px-1"></i>
+        <i className="bi bi-rewind-fill fs-4 px-1"></i>
         <i className="bi bi-play-fill fs-1 px-1"></i>
-        <i className="bi bi-skip-forward-fill fs-4 px-1"></i>
+        <i className="bi bi-fast-forward-fill fs-4 px-1"></i>
         <i className="bi bi-repeat fs-6 px-1"></i>
       </div>
       <div className="bg-secondary w-50 d-flex justify-content-center align-items-center">
